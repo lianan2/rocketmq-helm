ef6b263715ab06ac15ba4949b354082e691bca16
lianan <li_a_nan2@126.com>
2024-12-02 08:08:12 +0000

    update

--- README.md
+++ README.md
@@ -5,9 +5,13 @@
 sh run.sh
 ```
 
-访问dashboard（账号admin/admin）  
+访问dashboard（账号admin/admin)
+```
 http://{ADDRESS}:38080/
+```
 
 连接rocketmq:  
+```
 地址 {ADDRESS}:9876  
 账号 user01/user01
+```
--- run.sh
+++ run.sh
@@ -1,24 +1,3 @@
 # 部署
 kubectl create ns rocketmq
 cd rocketmq && helm install rocketmq . -n rocketmq
-
-#namespace/rocketmq created
-#NAME: rocketmq
-#LAST DEPLOYED: Mon Dec  2 07:07:29 2024
-#NAMESPACE: rocketmq
-#STATUS: deployed
-#REVISION: 1
-#TEST SUITE: None
-#NOTES:
-#>>> Nameserver Address:
-#    rocketmq-nameserver.rocketmq.svc:9876
-#
-#>>> Proxy Remoting Address:
-#    rocketmq-proxy.rocketmq.svc:8080
-#
-#>>> Proxy gRPC Address:
-#    rocketmq-proxy.rocketmq.svc:8081
-#
-#>>> RocketMQ Dashboard Auth:
-#      username: admin   password: admin
-#      username: user01  password: user01
